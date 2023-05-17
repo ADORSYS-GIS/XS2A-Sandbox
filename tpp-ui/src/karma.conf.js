883e03f1c0b7b6914789a71c33dab1316382c1ce
sah <sajjad.heidarzadeh@adorsys.com>
2023-05-17 15:55:18 +0200

    change karma config

--- tpp-ui/src/karma.conf.js
+++ tpp-ui/src/karma.conf.js
@@ -9,7 +9,7 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
+      require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma'),
       require('karma-firefox-launcher'),
     ],
@@ -19,9 +19,9 @@ module.exports = function (config) {
       },
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
+    coverageReporter: {
       dir: require('path').join(__dirname, '../coverage'),
-      reports: ['html', 'lcovonly'],
+      reporters: ['html', 'lcovonly'],
       fixWebpackSourcePaths: true,
     },
     reporters: ['progress', 'kjhtml'],
