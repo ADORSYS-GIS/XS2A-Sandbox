696f3fc61e53298af7930c6fad8d9b77410b89a9
sah <sajjad.heidarzadeh@adorsys.com>
2023-06-14 16:32:12 +0200

    fix oba customization test

--- oba-ui/src/app/common/services/customize.service.spec.ts
+++ oba-ui/src/app/common/services/customize.service.spec.ts
@@ -94,7 +94,7 @@ describe('CustomizeService', () => {
     });
 
     const req = httpTestingController.expectOne(
-      '../assets/UI/defaultTheme.json'
+      '../assets/UI/custom/UITheme.json'
     );
     expect(req.request.method).toEqual('GET');
   });
@@ -160,7 +160,7 @@ describe('CustomizeService', () => {
     expect(typeof service.getLogo()).toBe('string');
   });
 
-  it('should change font',  (done) => {
+  it('should change font', (done) => {
     service.setUserTheme({
       ...defTheme,
       globalSettings: {
@@ -185,8 +185,8 @@ describe('CustomizeService', () => {
     setTimeout(() => {
       const tmp = getComputedStyle(document.body).getPropertyValue(
         '--fontFamily'
-      );
-      expect(tmp).toEqual(' Verdana, sans-serif');
+      ).trim();
+      expect(tmp).toEqual('Verdana, sans-serif');
       done();
     }, 100);
   });
