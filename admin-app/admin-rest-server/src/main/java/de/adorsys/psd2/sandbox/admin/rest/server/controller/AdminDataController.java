b32501103ec5b5a521e52c41a42414036b65a0c7
Hryhorii Hevorkian <hhe@adorsys.com.ua>
2024-01-05 12:04:06 +0200

    pmd fix.

--- admin-app/admin-rest-server/src/main/java/de/adorsys/psd2/sandbox/admin/rest/server/controller/AdminDataController.java
+++ admin-app/admin-rest-server/src/main/java/de/adorsys/psd2/sandbox/admin/rest/server/controller/AdminDataController.java
@@ -21,7 +21,6 @@ package de.adorsys.psd2.sandbox.admin.rest.server.controller;
 import de.adorsys.psd2.sandbox.admin.rest.api.resource.AdminDataRestApi;
 import de.adorsys.psd2.sandbox.admin.rest.server.service.IbanGenerationService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
